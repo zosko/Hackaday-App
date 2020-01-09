a621aa9a3da52a3740b3a1a57d9a7fa101cc6d5a
bosko <bosko.petreski@gmail.com>
2020-01-09 13:31:48 +0100

    Initial Commit

---
+++ Hackaday_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hackaday_App
+//
+//  Created by Bosko Petreski on 1/9/20.
+//  Copyright © 2020 Bosko Petreski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
